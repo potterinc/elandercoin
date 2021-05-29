a88639ef31e77a064e34844bfefd9c3629bc28dd
potterinc <harry_p30@yahoo.com>
2021-05-29 23:13:15 +0100

    UI: Investment page done

--- dashboard/js/jquery.copyrights.js
+++ dashboard/js/jquery.copyrights.js
@@ -1,18 +1,18 @@
-(function($){
-	
+(function ($) {
+
 	// copyrigths year
-	$.fn.copyrights = ()=>{
-		
+	$.fn.copyrights = () => {
+
 		// to use this for variable selectors
-		return this.each(function(){
+		return this.each(function () {
 			let d = new Date();
 			return $(this).html(d.getFullYear());
 		});
 	};
-	
+
 	// Developer
-	$.fn.developer = ()=>{
-		return this.each(function(){
+	$.fn.developer = () => {
+		return this.each(function () {
 			$(this).html('<a href="https://potterincorporated.com" target="_blank">Potter Inc&trade;</a>');
 		})
 	}
